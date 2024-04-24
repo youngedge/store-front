d6673df4b52e60d0555ed4ec74eb759749231329
Oti Desmond <desmondotid.don16@gmail.com>
2024-04-24 11:46:15 +0100

    "Adjust heights of management and orders components, add CSS for orders component, and update Sidebar CSS"
    This commit includes the following changes:
    
    * The height of the management component is adjusted from 890px to

--- src/components/Orders/orders.tsx
+++ src/components/Orders/orders.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import Sidebar from "../Sidebar/Sidebar";
 import Navbar from "../Navbar/Navbar";
-
+import "./orders.css";
 
 const orders = () => {
     return (
         <div className="Container" >
             <Sidebar />
             <Navbar />
+            <div className="orders-widget1">
+                <h2 className="Orders-text1">Overall Orders</h2>
+                </div>
+                <div className="orders-widget2">
+                <h2 className="Orders-text2">Orders</h2>
+                    </div>
 </div>
     );
 }
